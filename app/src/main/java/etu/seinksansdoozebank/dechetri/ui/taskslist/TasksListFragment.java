991046399d5c7797512b4de2630527a98194c529
Baptiste LACROIX <baptiste.lacroix03@gmail.com>
2024-03-25 21:03:22 +0100

    feat: adding the task list view. #8

--- app/src/main/java/etu/seinksansdoozebank/dechetri/ui/taskslist/TasksListFragment.java
+++ app/src/main/java/etu/seinksansdoozebank/dechetri/ui/taskslist/TasksListFragment.java
@@ -1,32 +1,44 @@
 package etu.seinksansdoozebank.dechetri.ui.taskslist;
 
-import androidx.lifecycle.ViewModelProvider;
-
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ListView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.TextView;
+import java.util.ArrayList;
+import java.util.List;
 
 import etu.seinksansdoozebank.dechetri.databinding.FragmentTasksListBinding;
+import etu.seinksansdoozebank.dechetri.model.taskslist.Task;
 
-public class TasksListFragment extends Fragment {
+public class TasksListFragment extends Fragment implements TasksListAdapterListener {
     private FragmentTasksListBinding binding;
+    private ListView listViewTasks;
+    private List<Task> taskList;
+    private TasksListAdapter taskListAdapter;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        TasksListViewModel tasksListViewModel =
-                new ViewModelProvider(this).get(TasksListViewModel.class);
-
         binding = FragmentTasksListBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textTasksList;
-        tasksListViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        listViewTasks = binding.listViewTasks;
+
+        // Create a list of flux
+        taskList = new ArrayList<>();
+        taskList.add(new Task("Task 1", "Waste type 1", "Address 1"));
+        taskList.add(new Task("Task 2", "Waste type 2", "Address 2"));
+        taskList.add(new Task("Task 3", "Waste type 3", "Address 3"));
+        taskList.add(new Task("Task 4", "Waste type 4", "Address 4"));
+        taskList.add(new Task("Task 5", "Waste type 5", "Address 5"));
+
+        // Create an adapter
+        taskListAdapter = new TasksListAdapter(getActivity(), taskList);
+        listViewTasks.setAdapter(taskListAdapter);
 
         return root;
     }
