3691f4f4400132f110c601e3a7a349de5f4c99ed
Antoine-FdRg <antoine.fadda-rodriguez@etu.unice.fr>
2024-03-25 13:33:14 +0100

    :sparkles: link all component to nav bar #3

--- app/src/main/java/etu/seinksansdoozebank/dechetri/ui/wastereport/WasteReportFragment.java
+++ app/src/main/java/etu/seinksansdoozebank/dechetri/ui/wastereport/WasteReportFragment.java
@@ -5,7 +5,6 @@ import androidx.lifecycle.ViewModelProvider;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
