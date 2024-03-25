8a8269f3c32cbb4757ca266054df352d08c6650a
Antoine-FdRg <antoine.fadda-rodriguez@etu.unice.fr>
2024-03-25 22:32:43 +0100

    :sparkles: create nav bar depending on chosen role #3

--- app/src/main/java/etu/seinksansdoozebank/dechetri/RoleChoiceActivity.java
+++ app/src/main/java/etu/seinksansdoozebank/dechetri/RoleChoiceActivity.java
@@ -3,6 +3,7 @@ package etu.seinksansdoozebank.dechetri;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
@@ -33,18 +34,26 @@ public class RoleChoiceActivity extends AppCompatActivity {
         employeeButton = findViewById(R.id.btnEmployee);
         managerButton = findViewById(R.id.btnManager);
         adminButton = findViewById(R.id.btnAdmin);
-        userButton.setOnClickListener(v -> launchApp("user"));
-        employeeButton.setOnClickListener(v -> launchApp("employee"));
-        managerButton.setOnClickListener(v -> launchApp("manager"));
-        adminButton.setOnClickListener(v -> launchApp("admin"));
+        userButton.setOnClickListener(v -> launchApp(getString(R.string.role_user_title)));
+        employeeButton.setOnClickListener(v -> launchApp(getString(R.string.role_employee_title)));
+        managerButton.setOnClickListener(v -> launchApp(getString(R.string.role_manager_title)));
+        adminButton.setOnClickListener(v -> launchApp(getString(R.string.role_admin_title)));
     }
 
     private void launchApp(String role) {
-        //add role to shared preferences
-        SharedPreferences sharedPref = getPreferences(MODE_PRIVATE);
-        SharedPreferences.Editor editor = sharedPref.edit();
+        SharedPreferences sharedPreferences = getSharedPreferences(getString(R.string.shared_preferences_file_key), MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
         editor.putString(getString(R.string.shared_preferences_key_role), role);
         editor.apply();
+
+        // Verify that the role has been saved
+        String savedRole = sharedPreferences.getString(getString(R.string.shared_preferences_key_role), "");
+        if (role.equals(savedRole)) {
+            Log.d("RoleChoiceActivity", "Role saved: " + role);
+        } else {
+            Log.d("RoleChoiceActivity", "Failed to save role");
+            return;
+        }
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
     }
