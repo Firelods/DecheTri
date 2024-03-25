43a2afa08aae05466a9993455dae7bea23ce67cf
Antoine-FdRg <antoine.fadda-rodriguez@etu.unice.fr>
2024-03-25 13:26:23 +0100

    :sparkles: link all component to nav bar #3

--- app/src/main/java/etu/seinksansdoozebank/dechetri/ui/wastemap/WasteMapViewModel.java
+++ app/src/main/java/etu/seinksansdoozebank/dechetri/ui/wastemap/WasteMapViewModel.java
@@ -1,7 +1,18 @@
 package etu.seinksansdoozebank.dechetri.ui.wastemap;
 
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
 public class WasteMapViewModel extends ViewModel {
-    // TODO: Implement the ViewModel
-}
+
+    private final MutableLiveData<String> mText;
+
+    public WasteMapViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("Fragment des cartes de déchets");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }}
