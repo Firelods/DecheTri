ba6b62e85d38612217d507e9df1f1bb1b628107d
Firelods <clement.lefevre1@etu.unice.fr>
2024-03-27 17:04:19 +0100

    :recycle: implement wastelist #11

--- app/src/main/java/etu/seinksansdoozebank/dechetri/ui/wastemap/WasteMapFragment.java
+++ app/src/main/java/etu/seinksansdoozebank/dechetri/ui/wastemap/WasteMapFragment.java
@@ -40,7 +40,7 @@ public class WasteMapFragment extends Fragment {
     private FragmentWasteMapBinding binding;
     private MapView map;
     private IMapController mapController;
-    private static final String TAG = "WasteMapFragment";
+    private static final String  TAG = "WasteMapFragment";
     private LocationManager locationManager;
     Drawable markerDrawable;
 
@@ -60,15 +60,10 @@ public class WasteMapFragment extends Fragment {
             Log.v(TAG, "Location permission granted");
             setupMapView();
             updateMapToCurrentLocation();
-            List<Waste> wastes = new ArrayList<>();
 
-            // Ajouter des éléments Waste à la liste
-            wastes.add(new Waste("Homme", 43.568649561971746, 7.118045347165924, "52, avenue Laure Tessier\n02448 Briand-sur-Renard"));
-            wastes.add(new Waste("Histoire", 43.5903290982534, 7.144700577473443, "634, chemin Thibault Lévy\n94291 Vallet-sur-Carpentier"));
-            wastes.add(new Waste("Village", 43.584942163317315, 7.129661435858396, "54, chemin Valentine Martineau\n48705 Bruneauboeuf"));
-            wastes.add(new Waste("Midi", 43.59516846342956, 7.123057751195417, "735, rue Inès Munoz\n94926 Lejeune-sur-Mer"));
-            wastes.add(new Waste("Rideau", 43.594965510989326, 7.124292537096873, "avenue de Moulin\n66315 Da Costa"));
-            addWastePointsOnMap(wastes);
+            WasteList wasteList = new WasteList();
+            addWastePointsOnMap(wasteList);
+
         } else {
             Log.v(TAG, "Location permission not granted");
             requestLocationPermission();
