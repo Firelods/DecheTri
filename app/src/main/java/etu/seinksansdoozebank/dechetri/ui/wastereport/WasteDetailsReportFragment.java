8ff437dd2b3e9d09dd0fca08e04eaaaf81cbe38d
Emma Allain <emma.allain@etu.univ-cotedazur.fr>
2024-03-31 13:43:34 +0200

    feat(view): Ajout de la localisation actuelle  dans editText   #6

--- app/src/main/java/etu/seinksansdoozebank/dechetri/ui/wastereport/WasteDetailsReportFragment.java
+++ app/src/main/java/etu/seinksansdoozebank/dechetri/ui/wastereport/WasteDetailsReportFragment.java
@@ -1,66 +1,69 @@
 package etu.seinksansdoozebank.dechetri.ui.wastereport;
 
-import android.os.Bundle;
 
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
+import android.widget.Button;
+import android.widget.EditText;
+import android.Manifest;
 import etu.seinksansdoozebank.dechetri.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link WasteDetailsReportFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+
 public class WasteDetailsReportFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public WasteDetailsReportFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment WasteDetailsReportFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static WasteDetailsReportFragment newInstance(String param1, String param2) {
-        WasteDetailsReportFragment fragment = new WasteDetailsReportFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+    private EditText locationEditText;
+    private LocationManager locationManager;
+    private LocationListener locationListener;
+    private Button confirmButton;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_waste_details_report, container, false);
+        View view = inflater.inflate(R.layout.fragment_waste_details_report, container, false);
+
+        locationEditText = view.findViewById(R.id.LocalisationInput);
+        locationManager = (LocationManager) requireActivity().getSystemService(Context.LOCATION_SERVICE);
+
+        //On regarde si l'utilisateur a donné les permissions pour le service de localisation à l'application
+        if (ActivityCompat.checkSelfPermission(requireContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
+                ActivityCompat.checkSelfPermission(requireContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+
+            ActivityCompat.requestPermissions(requireActivity(), new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION}, 1);
+        } else {
+            //On récupère la localisation du téléphone
+            Location lastKnownLocation = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+            updateLocation(lastKnownLocation);
+        }
+
+        confirmButton = view.findViewById(R.id.ValidateButton);
+        confirmButton.setOnClickListener(v -> {
+            //TODO : ajouter le déchet à la liste des déchets en ayant enregistrer toutes les informations actuelles / passer au fragment d'après
+        });
+
+        return view;
     }
+
+    /*
+        Ajout de la localisation sur l'EditText
+     */
+    private void updateLocation(Location location) {
+        if (location != null) {
+            double latitude = location.getLatitude();
+            double longitude = location.getLongitude();
+            String locationString = latitude + "," + longitude;
+            locationEditText.setText(locationString);
+        }
+    }
+
 }
