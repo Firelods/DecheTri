9f17f4a45fc775ba819669b336347350affb1d73
Firelods <clement.lefevre1@etu.unice.fr>
2024-04-08 19:29:32 +0200

    :ambulance: fix map fragment on permissions

--- app/src/main/java/etu/seinksansdoozebank/dechetri/ui/wastemap/WasteMapFragment.java
+++ app/src/main/java/etu/seinksansdoozebank/dechetri/ui/wastemap/WasteMapFragment.java
@@ -11,6 +11,7 @@ import android.content.Context;
 import android.content.pm.PackageManager;
 import android.graphics.drawable.Drawable;
 import android.location.Location;
+import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
 
@@ -36,13 +37,14 @@ import java.util.List;
 import etu.seinksansdoozebank.dechetri.R;
 import etu.seinksansdoozebank.dechetri.databinding.FragmentWasteMapBinding;
 
-public class WasteMapFragment extends Fragment {
+public class WasteMapFragment extends Fragment implements LocationListener {
     private FragmentWasteMapBinding binding;
     private MapView map;
     private IMapController mapController;
     private static final String TAG = "WasteMapFragment";
     private LocationManager locationManager;
     Drawable markerDrawable;
+    private ActivityResultLauncher<String> requestPermissionLauncher;
 
     @Nullable
     @Override
@@ -50,68 +52,47 @@ public class WasteMapFragment extends Fragment {
         binding = FragmentWasteMapBinding.inflate(inflater, container, false);
         View view = binding.getRoot();
 
-        Context ctx = getContext();
-        if (ctx != null) {
-            markerDrawable = ContextCompat.getDrawable(getContext(), R.drawable.my_location);
-            Configuration.getInstance().load(ctx, ctx.getSharedPreferences("osmdroid", Context.MODE_PRIVATE));
-            locationManager = (LocationManager) ctx.getSystemService(Context.LOCATION_SERVICE);
-        }
+        initializeLocationManager();
+
+        requestPermissionLauncher = registerForActivityResult(new ActivityResultContracts.RequestPermission(), isGranted -> {
+            if (isGranted) {
+                Log.v(TAG, "Location permission granted");
+                setupMapView();
+                updateMapToCurrentLocation();
+                WasteList wasteList = new WasteList();
+                addWastePointsOnMap(wasteList);
+            } else {
+                Log.v(TAG, "Location permission denied");
+            }
+        });
+
         if (checkLocationPermission()) {
-            Log.v(TAG, "Location permission granted");
             setupMapView();
             updateMapToCurrentLocation();
-
             WasteList wasteList = new WasteList();
             addWastePointsOnMap(wasteList);
-
         } else {
-            Log.v(TAG, "Location permission not granted");
             requestLocationPermission();
         }
-
 
         return view;
     }
 
-    @Override
-    public void onPause() {
-        super.onPause();
-        map.onPause();
-    }
-
-    @Override
-    public void onResume() {
-        super.onResume();
-        map.onResume();
-        if (checkLocationPermission()) {
-            updateMapToCurrentLocation();
+    private void initializeLocationManager() {
+        Context ctx = getContext();
+        if (ctx != null) {
+            markerDrawable = ContextCompat.getDrawable(ctx, R.drawable.my_location);
+            Configuration.getInstance().load(ctx, ctx.getSharedPreferences("osmdroid", Context.MODE_PRIVATE));
+            locationManager = (LocationManager) ctx.getSystemService(Context.LOCATION_SERVICE);
         }
-    }
-
-    @Override
-    public void onDestroy() {
-        super.onDestroy();
-        map.onDetach();
     }
 
     private boolean checkLocationPermission() {
         Context context = getContext();
-        if (context == null) return false;
-
-        return ContextCompat.checkSelfPermission(context, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED
-                && ContextCompat.checkSelfPermission(context, Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED;
+        return context != null &&
+                ContextCompat.checkSelfPermission(context, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED &&
+                ContextCompat.checkSelfPermission(context, Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED;
     }
-
-    private final ActivityResultLauncher<String> requestPermissionLauncher =
-            registerForActivityResult(new ActivityResultContracts.RequestPermission(), isGranted -> {
-                if (isGranted) {
-                    setupMapView();
-                    updateMapToCurrentLocation();
-                } else {
-                    // Handle the case where permission is denied
-                    Log.v(TAG, "Location permission denied");
-                }
-            });
 
     private void requestLocationPermission() {
         requestPermissionLauncher.launch(Manifest.permission.ACCESS_FINE_LOCATION);
@@ -124,12 +105,17 @@ public class WasteMapFragment extends Fragment {
         mapController = map.getController();
     }
 
+
+
+
     private void updateMapToCurrentLocation() {
         if (ActivityCompat.checkSelfPermission(requireContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
                 && ActivityCompat.checkSelfPermission(requireContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             return;
         }
-
+        if (getContext() != null && checkLocationPermission()) {
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, this);
+        }
         Location lastKnownLocation = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
         Log.v(TAG, "Last known location from GPS provider: " + lastKnownLocation);
         if (lastKnownLocation == null) {
@@ -137,18 +123,17 @@ public class WasteMapFragment extends Fragment {
         }
         Log.v(TAG, "Last known location: " + lastKnownLocation);
         if (lastKnownLocation != null) {
-            GeoPoint startPoint = new GeoPoint(lastKnownLocation.getLatitude(), lastKnownLocation.getLongitude());
-            mapController.setCenter(startPoint);
-            addMarker(startPoint);
-            mapController.setZoom(15.0);
+            setLocationOnPoint(lastKnownLocation);
         } else {
-            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, location -> {
-                GeoPoint startPoint = new GeoPoint(location.getLatitude(), location.getLongitude());
-                mapController.setCenter(startPoint);
-                mapController.setZoom(15.0);
-                addMarker(startPoint);
-            });
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, this::setLocationOnPoint);
         }
+    }
+
+    private void setLocationOnPoint(Location lastKnownLocation) {
+        GeoPoint startPoint = new GeoPoint(lastKnownLocation.getLatitude(), lastKnownLocation.getLongitude());
+        mapController.setCenter(startPoint);
+        mapController.setZoom(15.0);
+        addMarker(startPoint);
     }
 
     private void addMarker(GeoPoint startPoint) {
@@ -216,5 +201,53 @@ public class WasteMapFragment extends Fragment {
         wasteDialogFragment.setArguments(args);
 
         wasteDialogFragment.show(getParentFragmentManager(), "wasteDetails");
+    }
+    @Override
+    public void onLocationChanged(@NonNull Location location) {
+        setLocationOnPoint(location);
+    }
+
+    @Override
+    public void onLocationChanged(@NonNull List<Location> locations) {
+        LocationListener.super.onLocationChanged(locations);
+    }
+
+    @Override
+    public void onFlushComplete(int requestCode) {
+        LocationListener.super.onFlushComplete(requestCode);
+    }
+
+    @Override
+    public void onProviderEnabled(@NonNull String provider) {
+        LocationListener.super.onProviderEnabled(provider);
+    }
+
+    @Override
+    public void onProviderDisabled(@NonNull String provider) {
+        LocationListener.super.onProviderDisabled(provider);
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        map.onPause();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        map.onResume();
+        if (checkLocationPermission()) {
+            updateMapToCurrentLocation();
+        }
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        if (locationManager != null) {
+            locationManager.removeUpdates(this);
+        }
+        map.onDetach();
     }
 }
