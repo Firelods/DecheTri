f378ee083776c593815c0ad6116688693bbf89bf
Antoine-FdRg <antoine.fadda-rodriguez@etu.unice.fr>
2024-05-14 15:24:32 +0200

    :recycle: fix PR remove static import #62

--- app/src/main/java/etu/seinksansdoozebank/dechetri/MainActivity.java
+++ app/src/main/java/etu/seinksansdoozebank/dechetri/MainActivity.java
@@ -25,7 +25,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        etu.seinksansdoozebank.dechetri.databinding.ActivityMainBinding binding = ActivityMainBinding.inflate(getLayoutInflater());
+        ActivityMainBinding binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
         configNavigation();
