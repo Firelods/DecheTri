12584c8fa1f65239d011ffea79a7ce82d443d20c
Emma Allain <emma.allain@etu.univ-cotedazur.fr>
2024-05-14 15:22:16 +0200

    :ambulance: deleted empty files

--- app/src/main/java/etu/seinksansdoozebank/dechetri/ui/taskslist/TasksListViewModel.java
+++
@@ -1,7 +0,0 @@
-package etu.seinksansdoozebank.dechetri.ui.taskslist;
-
-import androidx.lifecycle.ViewModel;
-
-public class TasksListViewModel extends ViewModel {
-    // TODO: Implement the ViewModel
-}
