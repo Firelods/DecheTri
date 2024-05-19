e6dd604d36b72a656e95b8d5dacad5592c6feb0d
Antoine-FdRg <antoine.fadda-rodriguez@etu.unice.fr>
2024-05-19 14:43:16 +0200

    :building_construction: use a fragment to show the dialog #59

---
+++ app/src/main/java/etu/seinksansdoozebank/dechetri/ui/flux/NewAnnouncementFragmentDialog.java
@@ -0,0 +1,228 @@
+package etu.seinksansdoozebank.dechetri.ui.flux;
+
+import android.app.Activity;
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
+import android.content.Context;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.DialogFragment;
+
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
+
+import etu.seinksansdoozebank.dechetri.R;
+import etu.seinksansdoozebank.dechetri.controller.api.APIController;
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.Response;
+
+/**
+ * A simple {@link DialogFragment} subclass.
+ * Use the {@link NewAnnouncementFragmentDialog#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class NewAnnouncementFragmentDialog extends DialogFragment {
+    private final String TAG = "512Bank " + getClass().getSimpleName();
+
+    private Context context;
+    private Activity activity;
+
+    private TextView tv_date_label;
+    private EditText et_date;
+    private EditText et_title;
+    private EditText et_description;
+    private Button btn_add_date;
+    private Button publishButton;
+
+    private Calendar pickedDate;
+
+    public NewAnnouncementFragmentDialog() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @return A new instance of fragment NewAnnouncementFragmentDialog.
+     */
+    public static NewAnnouncementFragmentDialog newInstance() {
+        NewAnnouncementFragmentDialog fragment = new NewAnnouncementFragmentDialog();
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public void onAttach(@NonNull Context context) {
+        super.onAttach(context);
+        activity = requireActivity();
+        Log.d(TAG, "onAttach: " + activity.getLocalClassName());
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_new_announcement_dialog, container, false);
+
+        context = requireContext();
+
+
+        et_title = view.findViewById(R.id.etxt_title);
+        et_description = view.findViewById(R.id.etxt_description);
+        tv_date_label = view.findViewById(R.id.tv_date_label);
+        btn_add_date = view.findViewById(R.id.btn_add_date);
+        et_date = view.findViewById(R.id.et_date);
+        publishButton = view.findViewById(R.id.btn_publish);
+        Button cancel = view.findViewById(R.id.btn_cancel);
+
+        assert et_date != null;
+        et_date.setOnTouchListener((v, event) -> {
+            if (event.getAction() == android.view.MotionEvent.ACTION_UP) {
+                showDateTimePicker();
+            }
+            return true;
+        });
+        if (btn_add_date != null) {
+            btn_add_date.setOnClickListener(v1 -> {
+                if (tv_date_label != null && et_date != null) {
+                    if (tv_date_label.getVisibility() == View.GONE) {
+                        showDateTimePicker();
+                    } else {
+                        changeDateInfoVisibility(false);
+                    }
+                }
+            });
+        } else {
+            throw new RuntimeException("btn_add_date is null");
+        }
+        publishButton.setEnabled(false);
+        setupTextWatcher();
+        Log.d(TAG, "showNewAnnouncementDialog: " + publishButton.isEnabled());
+        publishButton.setOnClickListener(v -> {
+            String title;
+            String description;
+            if (et_title != null && et_description != null && et_date != null) {
+                title = et_title.getText().toString();
+                description = et_description.getText().toString();
+                publishAnnouncement(title, description, pickedDate);
+                this.dismiss();
+            }
+        });
+        cancel.setOnClickListener(v -> this.dismiss());
+
+
+        return view;
+    }
+
+    private void changeDateInfoVisibility(boolean visible) {
+        if (visible) {
+            tv_date_label.setVisibility(View.VISIBLE);
+            et_date.setVisibility(View.VISIBLE);
+            btn_add_date.setText(R.string.remove_date_text);
+        } else {
+            btn_add_date.setText(R.string.add_date_text);
+            tv_date_label.setVisibility(View.GONE);
+            et_date.setVisibility(View.GONE);
+            pickedDate = null;
+        }
+    }
+
+    public void showDateTimePicker() {
+        final Calendar currentDate = Calendar.getInstance();
+        pickedDate = Calendar.getInstance();
+        new DatePickerDialog(context, (view, year, monthOfYear, dayOfMonth) -> {
+            pickedDate.set(year, monthOfYear, dayOfMonth);
+            new TimePickerDialog(context, (view1, hourOfDay, minute) -> {
+                pickedDate.set(Calendar.HOUR_OF_DAY, hourOfDay);
+                pickedDate.set(Calendar.MINUTE, minute);
+                changeDateInfoVisibility(true);
+                et_date.setText(new SimpleDateFormat("dd/MM/yyyy HH:mm", Locale.US).format(pickedDate.getTime()));
+            }, currentDate.get(Calendar.HOUR_OF_DAY), currentDate.get(Calendar.MINUTE), true).show();
+        }, currentDate.get(Calendar.YEAR), currentDate.get(Calendar.MONTH), currentDate.get(Calendar.DATE)).show();
+    }
+
+    private void setupTextWatcher() {
+        TextWatcher textWatcher = new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+                // Nothing to do
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                // Nothing to do
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                Log.d(TAG, "afterTextChanged: " + et_title.getText().toString().trim() + " " + et_description.getText().toString().trim());
+                publishButton.setEnabled(
+                        !et_title.getText().toString().trim().isEmpty() &&
+                                !et_description.getText().toString().trim().isEmpty()
+                );
+            }
+        };
+
+        et_title.addTextChangedListener(textWatcher);
+        et_description.addTextChangedListener(textWatcher);
+    }
+
+    private void publishAnnouncement(String title, String description, Calendar eventDate) {
+        Callback onResponse = new Callback() {
+            @Override
+            public void onFailure(@NonNull Call call, @NonNull IOException e) {
+                String message = e.getMessage();
+                Log.e("APIController", "Error while creating announcement : " + message);
+                activity.runOnUiThread(() -> Toast.makeText(activity.getApplicationContext(), "Erreur lors de la publication de l'annonce : " + message, Toast.LENGTH_SHORT).show());
+            }
+
+            @Override
+            public void onResponse(@NonNull Call call, @NonNull Response response) {
+                if (response.isSuccessful()) {
+                    activity.runOnUiThread(() -> {
+                        Toast.makeText(activity.getApplicationContext(), R.string.add_announcement_result_success, Toast.LENGTH_SHORT).show();
+                    });
+                } else {
+                    activity.runOnUiThread(() -> {
+                        try {
+                            assert response.body() != null;
+                            String body = response.body().string();
+                            Log.e("APIController", "Error while creating announcement : " + body);
+                            Toast.makeText(activity.getApplicationContext(), R.string.add_announcement_result_error + " : " + body, Toast.LENGTH_SHORT).show();
+                        } catch (IOException e) {
+                            throw new RuntimeException(e);
+                        }
+                    });
+                }
+            }
+        };
+        if (eventDate == null) {
+            APIController.createAnnouncementNews(title, description, onResponse);
+        } else {
+            SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'", Locale.US);
+            String formattedEventDate = sdf.format(new Date(eventDate.getTimeInMillis()));
+            APIController.createAnnouncementEvent(title, description, formattedEventDate, onResponse);
+        }
+    }
+}
