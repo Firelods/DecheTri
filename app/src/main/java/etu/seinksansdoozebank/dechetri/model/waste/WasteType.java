ceb3357058eb30a0e380a412ae35ae91ae4798eb
Antoine-FdRg <antoine.fadda-rodriguez@etu.unice.fr>
2024-05-21 21:23:56 +0200

    :sparkles: gather last data to create a waste #80

--- app/src/main/java/etu/seinksansdoozebank/dechetri/model/waste/WasteType.java
+++ app/src/main/java/etu/seinksansdoozebank/dechetri/model/waste/WasteType.java
@@ -1,13 +1,15 @@
 package etu.seinksansdoozebank.dechetri.model.waste;
 
+import java.util.Arrays;
+
 import etu.seinksansdoozebank.dechetri.R;
 
 public enum WasteType {
+    OTHER("Autre", R.drawable.waste),
     HOUSEHOLD("Ménager", R.drawable.waste),
     INDUSTRIAL("Industriel", R.drawable.waste),
     HAZARDOUS("Dangereux", R.drawable.waste),
-    GREEN("Vert", R.drawable.waste),
-    OTHER("Autre", R.drawable.waste);
+    GREEN("Vert", R.drawable.waste);
 
     private final String name;
     private final int icon;
@@ -22,5 +24,9 @@ public enum WasteType {
     }
     public int getIcon() {
         return icon;
+    }
+
+    public static String[] getNames() {
+        return Arrays.stream(WasteType.values()).map(WasteType::getName).toArray(String[]::new);
     }
 }
--- app/src/main/java/etu/seinksansdoozebank/dechetri/ui/wastereport/WasteDetailsReportFragment.java
+++ app/src/main/java/etu/seinksansdoozebank/dechetri/ui/wastereport/WasteDetailsReportFragment.java
@@ -1,6 +1,9 @@
 package etu.seinksansdoozebank.dechetri.ui.wastereport;
 
 
+import static android.content.Context.MODE_PRIVATE;
+
+import android.content.SharedPreferences;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.location.Address;
@@ -11,9 +14,12 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 import androidx.fragment.app.Fragment;
@@ -29,9 +35,13 @@ import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
 
 import etu.seinksansdoozebank.dechetri.R;
+import etu.seinksansdoozebank.dechetri.model.waste.WasteType;
 
 
 public class WasteDetailsReportFragment extends Fragment {
+
+    private String selectedWasteType;
+
     interface GeocodeCallback {
         void onAddressFound(String address);
 
@@ -49,9 +59,10 @@ public class WasteDetailsReportFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_waste_details_report, container, false);
         localisationInput = view.findViewById(R.id.LocalisationInput);
         TextView locationTextView = view.findViewById(R.id.locationText);
-        Button validateButton = view.findViewById(R.id.ValidateButton);
+        Button validateButton = view.findViewById(R.id.validateButton);
         EditText nameInput = view.findViewById(R.id.NameInput);
         EditText descriptionInput = view.findViewById(R.id.DescriptionInput);
+        Spinner wasteTypeSpinner = view.findViewById(R.id.typeSpinner);
         ImageView wasteImage=view.findViewById(R.id.imageChosen);
         skeleton = view.findViewById(R.id.skeletonLayout);
         skeleton.showSkeleton();
@@ -108,6 +119,35 @@ public class WasteDetailsReportFragment extends Fragment {
         nameInput.addTextChangedListener(textWatcher);
         descriptionInput.addTextChangedListener(textWatcher);
         localisationInput.addTextChangedListener(textWatcher);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(requireContext(), R.layout.spinner_item_20sp, WasteType.getNames());
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        wasteTypeSpinner.setAdapter(adapter);
+        wasteTypeSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parentView, View selectedItemView, int position, long id) {
+                selectedWasteType = WasteType.getNames()[position];
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parentView) {
+                selectedWasteType = WasteType.OTHER.getName();
+            }
+        });
+
+        SharedPreferences sharedPreferences = requireActivity().getSharedPreferences(getString(R.string.shared_preferences_file_key), MODE_PRIVATE);
+        String userId = sharedPreferences.getString(getString(R.string.shared_preferences_key_user_id), getString(R.string.default_id));
+        validateButton.setOnClickListener(v -> this.reportWaste(
+                nameInput.getText().toString(),
+                selectedWasteType,
+                descriptionInput.getText().toString(),
+                userId,
+                localisationInput.getText().toString(),
+                latitude,
+                longitude,
+                chosenImage
+        ));
+
         return view;
     }
 
@@ -126,5 +166,17 @@ public class WasteDetailsReportFragment extends Fragment {
                 callback.onError(e);
             }
         });
+    }
+
+    private void reportWaste(String name, String wasteType, String description, String userReporterID, String address, double latitude, double longitude, byte[] image) {
+        Log.d("WasteDetailsReportFragment", "{" +
+                "name='" + name + '\'' +
+                ", wasteType='" + wasteType + '\'' +
+                ", description='" + description + '\'' +
+                ", userReporterID='" + userReporterID + '\'' +
+                ", address='" + address + '\'' +
+                ", latitude=" + latitude +
+                ", longitude=" + longitude +
+                '}');
     }
 }
