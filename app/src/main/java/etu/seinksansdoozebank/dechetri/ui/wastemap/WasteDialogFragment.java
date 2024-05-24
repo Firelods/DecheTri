3230e9b277d3ac36bd8104301a946b3ee8d85d86
Emma Allain <emma.allain@etu.univ-cotedazur.fr>
2024-05-24 14:25:05 +0200

    :white_check_mark: added popup

--- app/src/main/java/etu/seinksansdoozebank/dechetri/ui/wastemap/WasteDialogFragment.java
+++ app/src/main/java/etu/seinksansdoozebank/dechetri/ui/wastemap/WasteDialogFragment.java
@@ -3,6 +3,8 @@ package etu.seinksansdoozebank.dechetri.ui.wastemap;
 import static android.content.Context.MODE_PRIVATE;
 
 import android.annotation.SuppressLint;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Bitmap;
@@ -125,27 +127,48 @@ public class WasteDialogFragment extends BottomSheetDialogFragment {
 
     private void configureDeleteButton(Button buttonDelete) {
         buttonDelete.setVisibility(View.VISIBLE);
-        buttonDelete.setOnClickListener(v -> APIController.deleteWaste(waste.getId(), new Callback() {
-            @Override
-            public void onFailure(@NonNull Call call, @NonNull IOException e) {
-                requireActivity().runOnUiThread(() -> Toast.makeText(getContext(), "Erreur lors de la suppression du déchet : " + e.getMessage(), Toast.LENGTH_SHORT).show());
-            }
-
-            @Override
-            public void onResponse(@NonNull Call call, @NonNull Response response) {
-                if (response.isSuccessful()) {
-                    requireActivity().runOnUiThread(() -> {
-                        Toast.makeText(getContext(), "Déchet supprimé avec succès.", Toast.LENGTH_SHORT).show();
-                        dismiss();
-                    });
-                } else {
-                    requireActivity().runOnUiThread(() -> Toast.makeText(getContext(), "Erreur lors de la suppression du déchet : " + response.message(), Toast.LENGTH_SHORT).show());
-                }
-            }
-        }));
+        buttonDelete.setOnClickListener(v->{
+            onClickDeleteWaste();
+        });
     }
 
+    public void onClickDeleteWaste() {
+        AlertDialog alertDialog = new AlertDialog.Builder(getContext())
+                .setTitle("Confirmation de suppression")
+                .setMessage("Etes-vous sur de vouloir supprimer ce signalement ? Ne le supprimer que s'il n'est pas sérieux, offensant ou hors de porté.")
+                .setPositiveButton("Supprimer", (dialog, which) -> {
+                    APIController.deleteWaste(waste.getId(), new Callback() {
+                        @Override
+                        public void onFailure(@NonNull Call call, @NonNull IOException e) {
+                            String message = e.getMessage();
+                            Log.e("APIController", "Error while removing waste : " + message);
+                            requireActivity().runOnUiThread(() -> Toast.makeText(getContext(),"Erreur lors de la suppression du déchet:"+e.getMessage(),Toast.LENGTH_SHORT).show());
+                        }
 
+                        @Override
+                        public void onResponse(@NonNull Call call, @NonNull Response response) {
+                            requireActivity().runOnUiThread(() -> {
+                                if (response.isSuccessful()) {
+                                    Toast.makeText(getContext(),"Déchet supprimé avec succès.",Toast.LENGTH_SHORT).show();
+                                    dismiss();
+                                } else {
+                                    Toast.makeText(getContext(),"Erreur lors de la suppression du déchet :"+response.message(),Toast.LENGTH_SHORT).show();
+                                }
+                            });
+                        }
+                    });
+                })
+                .setNegativeButton("Conserver", (dialog, which) -> {
+                    // Do nothing
+                })
+                .show();
+
+        Button deletionButton = alertDialog.getButton(DialogInterface.BUTTON_POSITIVE);
+        deletionButton.setTextColor(getResources().getColor(R.color.orange_600, null));
+        Button conservationButton = alertDialog.getButton(DialogInterface.BUTTON_NEGATIVE);
+        conservationButton.setBackgroundColor(getResources().getColor(R.color.green_700, null));
+        conservationButton.setTextColor(getResources().getColor(R.color.white_100, null));
+    }
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
 
